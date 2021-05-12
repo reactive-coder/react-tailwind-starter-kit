ef9c963f70ce06a8a3288938a941b27a85fe7b82
devarsh-patel <bhupendrabhai.patel2017@vitstudent.ac.in>
2021-05-12 17:44:48 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const App = () => (
+  <div className="h-screen w-screen flex justify-center items-center bg-white relative">
+    <div className="text-xl text-gray-900">
+      <div className="text-center">Customize this kit as per your needs !</div>
+      <div className="text-base w-96 text-center">
+        Use this kit to craft your amazing react application using tailwind css
+        framework. Let me know if you have any suggestions to improve this kit.
+      </div>
+    </div>
+    <div className="absolute m-auto w-max bottom-0">
+      For Developers, by Devarsh Patel.
+    </div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
